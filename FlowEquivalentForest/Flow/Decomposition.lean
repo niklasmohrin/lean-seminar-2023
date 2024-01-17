cd3ea349727a7eae0f8c095a0c91fc766327b3e4
postmartem <paula@marten-kern.de>
2024-01-17 15:35:59 +0100

    Teach computer more obvious things about Cycle Removal from Flows

--- FlowEquivalentForest/Flow/Decomposition.lean
+++ FlowEquivalentForest/Flow/Decomposition.lean
@@ -131,17 +131,23 @@ noncomputable def Flow.remove_all_cycles (F : Flow Pr) : Flow Pr :=
 termination_by Flow.remove_all_cycles F => ∑ u, ∑ v, F.f u v
 
 theorem Flow.remove_all_cycles.CycleFree (F : Flow Pr) : F.remove_all_cycles.CycleFree := sorry
+
 theorem Flow.remove_all_cycles.value (F : Flow Pr) : F.remove_all_cycles.value = F.value := sorry
+
 theorem Flow.remove_all_cycles.subset (F : Flow Pr) : F.remove_all_cycles ⊆ F := by
   have : Decidable (F.CycleFree) := Classical.dec _
   unfold Flow.remove_all_cycles
   if hF : F.CycleFree then
     simp only [dite_true, hF]
-    sorry -- Have not shown Flow Subset Relation to be reflexive
+    exact Eq.subset' rfl
   else
     let c := Classical.choice $ not_isEmpty_iff.mp $ Classical.choose_spec $ not_forall.mp hF
     simp only [dite_false, hF]
-    sorry
+    have : ∑ u, ∑ v, (F.remove_cycle c).f u v < ∑ u, ∑ v, F.f u v := sorry
+    have h1: remove_all_cycles (remove_cycle F c) ⊆  remove_cycle F c := by exact Flow.remove_all_cycles.subset (remove_cycle F c)
+    have h2 : remove_cycle F c ⊆ F := by exact Flow.remove_cycle.subset F c
+    exact subset_trans h1 h2
+termination_by Flow.remove_all_cycles.subset F => ∑ u, ∑ v, F.f u v
 
 def Flow.Walk.transfer {F F' : Flow Pr} (p : F.Walk s t) (h : F ⊆ F') : F'.Walk s t where
   val := p.val
